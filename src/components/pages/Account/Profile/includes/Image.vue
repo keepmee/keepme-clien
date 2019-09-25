bf933d1a94f82813ac5d3475f2e12ea6fc648074
Sofiane Akbly <sofiane.akbly@gmail.com>
2019-09-25 10:49:27 +0200

    Use component for profile picture

--- src/components/pages/Account/Profile/includes/Image.vue
+++ src/components/pages/Account/Profile/includes/Image.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="profile-image-container position-relative" v-if="user">
 
-    <triple-shape :size="130" :center-size="85" :strong="true" class="profile-image">
+    <!--<triple-shape :size="130" :center-size="85" :strong="true" class="profile-image">
       <div class="d-flex justify-content-center align-items-center h-100 w-100" :class="{'bg-color-1': !user.image}">
         <img :src="user.image" alt="Photo de profil" v-if="user.image">
         <i class="fal fa-user fa-4x text-white user-icon" v-else></i>
       </div>
-    </triple-shape>
+    </triple-shape>-->
+
+    <profile-picture :size="130" :center="user.image ? 130 : 85" class="profile-image" :strong="false"
+                     :image="{src: user.image, height: 130, width: 130}"/>
 
     <div class="profile-image-details">
-      <div class="bar main bg-color-3"></div>
+      <div class="bar main bg-color-3" :class="{'image': user.image}"></div>
     </div>
 
-    <div class="profile-image-name">
+    <div class="profile-image-name" :class="{'image': user.image}">
       <h3 class="sanchez">{{ getUserName() }}</h3>
     </div>
 
@@ -20,11 +23,12 @@
 </template>
 
 <script>
-    import TripleShape from "../../../../includes/commons/partials/TripleShape";
+    import TripleShape    from "../../../../includes/commons/partials/TripleShape";
+    import ProfilePicture from "../../../../includes/commons/Profile/Picture";
 
     export default {
         name      : "ProfileImageContainer",
-        components: { TripleShape },
+        components: { ProfilePicture, TripleShape },
         props     : {
             user       : Object || null,
             getUserName: { type: Function, required: true }
@@ -63,6 +67,17 @@
     height: 4px;
     width: 500px;
     max-width: calc(100% - 474px);
+  }
+
+  .profile-image-container .profile-image-details .bar.main.image {
+    top: 63px !important;
+    left: 135px !important;
+  }
+
+
+  .profile-image-container .profile-image-name.image {
+    top: 34px !important;
+    left: 135px;
   }
 
 </style>
