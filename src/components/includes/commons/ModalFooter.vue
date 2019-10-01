24f61753101a7fa0263a05b43a9b4b23c216624a
Sofiane Akbly <sofiane.akbly@gmail.com>
2019-10-01 11:52:33 +0200

    Bg transparent to button

--- src/components/includes/commons/ModalFooter.vue
+++ src/components/includes/commons/ModalFooter.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="vue-modal-footer pb-4">
     <div class="row w-100">
-      <div class="col-6 border-right d-flex justify-content-center align-items-center">
-        <button class="btn btn-block" type="button" @click.prevent="$emit('cancel')">
+      <div class="col-6 px-0 border-right d-flex justify-content-center align-items-center">
+        <button class="btn btn-block bg-transparent" type="button" @click.prevent="$emit('cancel')">
           <i class="fal fa-ban fa-2x text-danger"></i>
         </button>
       </div>
-      <div class="col-6 border-left d-flex justify-content-center align-items-center">
-        <button class="btn btn-block" type="submit" @click="$emit('save')">
+      <div class="col-6 px-0 border-left d-flex justify-content-center align-items-center">
+        <button class="btn btn-block bg-transparent" type="submit" @click="$emit('save')">
           <i class="fal fa-save fa-2x text-success"></i>
         </button>
       </div>
